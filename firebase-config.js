f021b2be1d5973860fa3d991d6ea9988ef81cdb6
Emanuel71836 <marcioemanuel959@gmail.com>
2025-07-11 12:24:55 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBgU2y-cl3n2HSl70jE4yhjQtFwGwC3cjw",
   authDomain: "work-mods.firebaseapp.com",
   projectId: "work-mods",
   storageBucket: "work-mods.firebasestorage.app",
   messagingSenderId: "215308060127",
-  appId: "1:215308060127:web:2ba923815e199ad817306a",
-  measurementId: "G-V2ZCG3JTRK"
+  appId: "1:215308060127:web:2ba923815e199ad817306a"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// AQUI ESTÁ A CORREÇÃO: Usamos 'export const' para que o login.html possa usar o objeto 'app'
+export const app = initializeApp(firebaseConfig);
